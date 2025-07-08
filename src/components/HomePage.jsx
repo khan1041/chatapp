213f7cad423a9a5036edb76bbd0a7b993bd29b18
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-07-08 23:55:32 +0600

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,13 +12,16 @@ function HomePage() {
   return (
     <div>
   
- <div className='flex gap-9 sm:h-[450px] overflow-scroll md:h-[550px] rounded-lg  bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+ {authUser?<div className='flex gap-9 sm:h-[450px] overflow-scroll md:h-[550px] rounded-lg  bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 <Sidebar/>
-
 <div className=' sm:h-[450px] h-[550px] bg-amber-200 overflow-scroll'>
    <MessageContainer/>
 
 </div>
+  </div>:<Login/>
+ 
+ }
+
   </div>
  
 
