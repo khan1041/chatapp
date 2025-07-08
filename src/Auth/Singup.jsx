fa27248d3f608501a79be01c4087ce2bce7e79a7
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-07-08 23:56:47 +0600

    Update Singup.jsx

--- src/Auth/Singup.jsx
+++ src/Auth/Singup.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`https://chatapp-backend-izd8.onrender.com/app/auth/register`, user, {
+      const res = await axios.post(`https://chatapp-backend1-1.onrender.com/app/auth/register`, user, {
         headers: {
           'Content-Type': 'application/json'
         },
